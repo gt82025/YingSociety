77b160bc13fe3f085a56fc8e493d6348376cb072
Wade <wade@begonia-design.com.tw>
2019-10-07 03:00:43 +0800

    優化社員傳記詹元 修改大事記部分內容顯示

--- src/js/history.js
+++ src/js/history.js
@@ -28,19 +28,19 @@ function historyPopup() {
         let regex = /\(([^)]+)\)/;
         let options = Style.match(regex)
         let imgSrc = options[0].substring(1, options[0].length - 1)
-        let dis = $(this).find('p').html();
+        //let dis = $(this).find('p').html();
         console.log(imgSrc);
-        console.log(dis);
+        //console.log(dis);
         $('.popupContent').removeClass(['op0', 'moveOut']);
         $('.popupContent img').attr('src', imgSrc)
-        $('.popupContent p').text(dis)
+        //$('.popupContent p').text(dis)
     })
     $('.popupContent .close').on("click", function() {
         $('.popupContent').addClass('op0');
         window.setTimeout(function() {
             $('.popupContent').addClass('moveOut');
             $('.popupContent img').attr('src', '');
-            $('.popupContent p').text('');
+            //$('.popupContent p').text('');
         }, 800)
 
     })
---
+++ src/js/members.js
@@ -0,0 +1,39 @@
+
+(function ($, window, document) {
+
+
+
+})(jQuery, window, document);
+$(window).on("load", function () {
+   $('p.profile').each(function(index,ele){
+    let text = $(this).get(0);
+    console.log(text);
+      if ($(window).width() <= scrL && $(window).width() >= scrS) {
+    $clamp(text, {
+      clamp: 5
+    });
+  } else if ($(window).width() < scrS) {
+    $clamp(text, {
+      clamp: 3
+    });
+  }
+   })
+
+ });
+
+  //使用clamp.js效果
+  //https://blog.csdn.net/chensonghuiyuan/article/details/49204747
+//   if ($(window).width() <= scrL && $(window).width() >= scrS) {
+//     $clamp($profileContent, {
+//       clamp: 5
+//     });
+//   } else if ($(window).width() < scrS) {
+//     $clamp($profileContent, {
+//       clamp: 3
+//     });
+//   }
+
+// });
+$(window).on("resize", function () {
+
+})
