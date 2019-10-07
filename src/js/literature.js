ff6014c1a21ca77507e13a6e6a948ff86bbd9008
Wade <wade@begonia-design.com.tw>
2019-10-08 01:02:06 +0800

    新增文獻單元與社員列表修改

---
+++ src/js/literature.js
@@ -0,0 +1,27 @@
+
+(function ($, window, document) {
+
+
+
+})(jQuery, window, document);
+$(window).on("load", function () {
+
+
+ });
+
+  //使用clamp.js效果
+  //https://blog.csdn.net/chensonghuiyuan/article/details/49204747
+//   if ($(window).width() <= scrL && $(window).width() >= scrS) {
+//     $clamp($profileContent, {
+//       clamp: 5
+//     });
+//   } else if ($(window).width() < scrS) {
+//     $clamp($profileContent, {
+//       clamp: 3
+//     });
+//   }
+
+// });
+$(window).on("resize", function () {
+
+})
