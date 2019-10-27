dc4cfed55a87f9dc09ab1d294b02382cfff0a675
Wade <wade@begonia-design.com.tw>
2019-10-28 03:30:03 +0800

    定期修改更新

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,8 @@ var $menuTurn;
 var $touchScop;
 var $BodyOutside;
 var $Device
+var $scrollTop;
+var $windowHeight
   // console.log(_uac.browser)     // returns browser name - ie. chrome, safari, ie10, edge, iemobile, etc.
   // console.log(_uac.device)     // returns device name  - ie. iphone, ipad, android, windows_phone
   // console.log(_uac.isiOS)      // returns if it's ios (boolean)
@@ -25,6 +27,7 @@ var $Device
 $(window).on("load", function () {
   console.log(_uac.browser);
   console.log(_uac.device);
+  $windowHeight = $(window).height(); 
   $Device = ['ie', 'iphone', 'ipad', 'android', 'windows_phone'];
   $menuTurn = $('.menuTurnBtn');
   $menuMenu = $('#mainmenu');
@@ -42,6 +45,8 @@ $(window).on("load", function () {
 
   }
 
+  menuFix(210,$(window).height()/2);
+
 
 
 
@@ -49,8 +54,14 @@ $(window).on("load", function () {
 
 });
 $(window).on("resize", function () {
+  $windowHeight = $(window).height(); 
   if ($(window).width() <= scrXL) {} else if ($(window).width() <= scrL) {} else if ($(window).width() <= scrM) {} else if ($(window).width() <= scrS) {}
 });
+$(window).on("scroll", function () {
+  menuFix(210,$(window).height()/2)
+
+
+})
 //主選單功能
 function menuActive() {
   $menuTurn.click(function () {
@@ -61,6 +72,22 @@ function menuActive() {
     $menuMenu.addClass('close');
     $touchScop.addClass('close');
   });
+}
+function menuFix($top,$showTop){
+  //console.log($(window).scrollTop());
+  $scrollTop = $(window).scrollTop();
+  if($scrollTop >= $top && $scrollTop <= $showTop){
+    $('header').addClass('fixed');
+  }else if( $scrollTop > $showTop){
+    $('header').addClass('show');
+    $('header').addClass('fixed');
+  }else if( $scrollTop < $top){
+    $('header').removeClass('show');
+    $('header').removeClass('fixed');
+  }
+
+
+
 }
 function smoothScroll(){
   $("html").easeScroll({
